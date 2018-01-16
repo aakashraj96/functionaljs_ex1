19dde74469e096b40af05e47c4c5e3e01a7e0e44
aakash_raj <aakash.raj5@gmail.com>
2018-01-16 12:36:33 +0530

    Function getShortMessages working without input validation

--- ex4.js
+++ ex4.js
@@ -1,5 +1,8 @@
 function getShortMessages(messages) {
-	return messages;
+	let result = messages.filter(function (element){ 
+		return element.message.length < 50;
+	});
+	return result;
 }
 
 let messages = [{message: 'hi'},{message: 'hello world'},{message: 'ash asdh jsdhgsdhfghjg ajghd fhjgdjhf jghsdfj gsdjfg sdhgfjhdgsjfgjdhgsf jdhsfjdg sghjf'}];
