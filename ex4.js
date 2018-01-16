59efaa1be3734ad8d6d4aa6f2eea3fd283b91652
aakash_raj <aakash.raj5@gmail.com>
2018-01-16 12:45:04 +0530

    returning array of strings instead of array of objects

--- ex4.js
+++ ex4.js
@@ -1,9 +1,11 @@
 function getShortMessages(messages) {
 	let result = messages.filter(function (element){ 
 		return element.message.length < 50;
-	});
+	}).map(x => x.message);
 	return result;
 }
 
 let messages = [{message: 'hi'},{message: 'hello world'},{message: 'ash asdh jsdhgsdhfghjg ajghd fhjgdjhf jghsdfj gsdjfg sdhgfjhdgsjfgjdhgsf jdhsfjdg sghjf'}];
 console.log('Check if getShortMessages call is working',getShortMessages(messages).length === 2);
+
+// module.exports = getShortMessages
