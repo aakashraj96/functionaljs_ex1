49aedc1046c1211b36f5cd07aeb3e930e7a58b29
aakash_raj <aakash.raj5@gmail.com>
2018-01-16 10:03:25 +0530

    symbols inside string passed

--- ex1.js
+++ ex1.js
@@ -5,3 +5,4 @@ function upperCaser(input)
 
 console.log('Testing the function',upperCaser('hello')==='HELLO');
 console.log('Numbers inside a string',upperCaser('123a')==='123A');
+console.log('Testing the function',upperCaser('!@ad')==='!@AD');
