0954de531f778fec30110716ddf2f69ac7d41338
aakash_raj <aakash.raj5@gmail.com>
2018-01-16 15:12:51 +0530

    Function call fixed

--- ex6.js
+++ ex6.js
@@ -1,11 +1,36 @@
+
+function reducer(accumulator, currentvalue)
+{
+	if(!Object.keys(accumulator).some( function (value)
+		{
+			return currentvalue === value;
+		})
+	)
+	{
+		accumulator[currentvalue] = 1;
+	}
+	else
+	{
+		accumulator[currentvalue]++;
+	}
+	return accumulator;
+	
+}
+
+
+
 function countWords(inputWords) 
 {
-	return inputWords;
+	let emptyObj = {};
+	let result = inputWords.reduce(reducer,emptyObj);
+	console.log(result);
+	return result;
 }
 
 module.exports = countWords
 
 let inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian'];
+
 
 
 console.log('Checking function call',Object.keys(countWords(inputWords)).length === 3);
