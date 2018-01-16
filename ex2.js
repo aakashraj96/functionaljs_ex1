0eba383ec0e380a18eba3365411c719cfa73eaaa
aakash_raj <aakash.raj5@gmail.com>
2018-01-16 12:01:09 +0530

    Function repeat testesd successful

--- ex2.js
+++ ex2.js
@@ -1,13 +1,25 @@
+let counter=0;
+
 function dummyf()
 {
-	
+	counter++;
 }
 
 
 function repeat(func,num)
 {
-
+   if(num === 0)
+   {
+   	// console.log('value of counter',counter);
+   	return counter;
+   }
+   else
+   {
+   	func();
+   	return repeat(func,--num);
+   }
 }
 
-
-console.log(repeat(dummyf,3)==3);
+ console.log('Testing return value of repeat',repeat(dummyf,3)===3);
+ counter = 0;
+ console.log('Testing what dummyf returns',dummyf() === 0);
